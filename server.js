3a04b37310b01b997f6b95073b6a9c4a88200d35
Ashir Rehman <ashir0845@gmail.com>
2024-12-19 22:08:13 +0530

    first version of our hotels

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const db = require("./db");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const Person = require("./models/Person");
+const MenuItem=require("./models/MenuItem")
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+const personRoutes=require('./routes/personRoutes');
+const menuRoutes=require('./routes/menuRoutes');
+
+app.use('/person',personRoutes);
+app.use('/menu',menuRoutes);
+
+app.listen(3000, () => {
+  console.log("server is live at 3000");
+});
