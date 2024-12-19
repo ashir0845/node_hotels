c3f0e5cdda0a49c610060d02d22a40f0962abc37
Ashir Rehman <ashir0845@gmail.com>
2024-12-19 23:19:35 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT=process.env.PORT || 3001;
 
 const Person = require("./models/Person");
 const MenuItem=require("./models/MenuItem")
@@ -18,6 +20,8 @@ const menuRoutes=require('./routes/menuRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log("server is live at 3000");
 });
